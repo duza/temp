59d63d9558430716887a15fb2ac7d2108b3b7f9b
duza <duza@tut.by>
2016-06-02 11:18:58 +0300

    Fixed tempserver for 10 synchronous processes with help fork

--- tempserv.py
+++ tempserv.py
@@ -1,25 +1,43 @@
 
 import socket
-
 import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 print "Server turn on. Listening port 2222..."
+count = 0# 
+childs = []#
 while True:
     conn, addr = s.accept()
-    print 'Connection is established {}:{}'.format(*addr)
-    while True:
-        data = conn.recv(1024)
-        if b'close' in data:
-            print 'Found "close" in data'
-            conn.close()
-            break
-        elif not data:
-            print 'No data.'
-            conn.close()
-            break
-        else:
-            print 'Data received: ', data
-            conn.send(data)
+    try:#
+        count += 1#
+        pid = os.fork()#
+    except:#
+        print 'error: create child'#
+        count -= 1#
+        os._exit(1)#
+    if pid == 0:#
+        print 'child: ', os.getpid()#
+        print 'Connection is established {}:{}'.format(*addr)
+        while True:
+            data = conn.recv(1024)
+            if b'close' in data:
+                print 'Found "close" in data'
+                conn.close()
+                break
+            elif not data:
+                print 'No data.'
+                conn.close()
+                break
+            else:
+                print 'Data received: ', data
+                conn.send(data)
+        os._exit(0)#
+    if pid > 0:#
+        print 'parent: ', os.getpid()#
+        childs.append(pid)#
+        for p in childs:#
+            pid, status = os.wait()#
+            print 'pid {} - status= {}'.format(pid,
+os.WEXITSTATUS(status))#
