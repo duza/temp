a8e680a6c75229497eab714537600fe3e994f93c
duza <pavel.mikhadziuk@gmail.com>
2026-07-12 15:42:54 +0200

    improve BFS

--- bfs.py
+++ bfs.py
@@ -1,8 +1,25 @@
+from collections import deque
+
 def bfs(graph, start):
-    visited, queue = set(), [start]
+    visited, queue = set(), deque([start])
     while queue:
-        vertex = queue.pop(0)
-        if vertex not in visited:
-            visited.add(vertex)
-            queue.extend(graph[vertex] - visited)
+        vertex = queue.popleft()
+
+        if vertex in visited:
+            continue
+
+        visited.add(vertex)
+        queue.extend(neighbor for neighbor in graph.get(vertex, ()) if neighbor not in visited)
     return visited
+
+# A -- B -- D
+#   \- C
+#       \- E
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D'},
+    'C': {'A', 'E'},
+    'D': {'B'},
+    'E': {'C'},
+}
+print(bfs(graph, 'A'))
